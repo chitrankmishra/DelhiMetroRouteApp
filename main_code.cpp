d9d23d32a2b398e10fad980750d7dec6b7e9ea25
Chitrank Mishra <37659873+chitrank0614@users.noreply.github.com>
2019-04-12 19:09:33 +0530

    Update main_code.cpp

--- main_code.cpp
+++ main_code.cpp
@@ -35,7 +35,24 @@ string makecapital(string str)
 
 void dijkstra(float graph[V][V], int src,int targ)
 {
-          //section incomplete
+    float dist[V];
+     bool sptSet[V];
+     for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+     dist[src] = 0;
+     for (int count = 0; count < V-1; count++)
+     {
+         int u = minDistance(dist, sptSet);
+       sptSet[u] = true;
+       for (int v = 0; v < V; v++)
+         if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX
+                                       && dist[u]+graph[u][v] < dist[v])
+            {
+                dist[v] = dist[u] + graph[u][v];
+                P[v].p.push_back(u);
+            }
+     }
+     printSolution(dist, V,src,targ);
 }
 
 
